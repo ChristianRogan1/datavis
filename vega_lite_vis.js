4ff51d0fb6435538296d68f6bf74fd3b84238a2e
ChristianRogan1 <crog0007@student.monash.edu>
2024-10-11 20:32:08 +1100

    starting to add text elements

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,13 @@
 var vg_1 = "visualisations/cropProductionLineChart.json";
 var vg_2 = "visualisations/co2ProductionChoropleth.json";
+var vg_3 = "Visualisations/cropTypeBarChart.json";
 
 
 vegaEmbed("#line_chart", vg_1).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#choropleth_map", vg_2).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
 }).catch(console.error);
