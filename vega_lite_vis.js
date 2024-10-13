e364e1dada9d6acd59c953aaf2fcbd12efc72248
ChristianRogan1 <crog0007@student.monash.edu>
2024-10-13 16:57:26 +1100

    Writing paragraphs

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,9 @@
 var vg_1 = "visualisations/cropProductionLineChart.json";
 var vg_2 = "visualisations/co2ProductionChoropleth.json";
 var vg_3 = "Visualisations/cropTypeBarChart.json";
+var vg_4 = "visualisations/IrragationAccessChoropleth.json";
+var vg_5 = "visualisations/economicImpactExtremeWeather.json"
+
 
 
 vegaEmbed("#line_chart", vg_1).then(function(result) {
@@ -10,4 +13,10 @@ vegaEmbed("#choropleth_map", vg_2).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#bar_chart", vg_3).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#irrigation_choropleth_map", vg_4).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#Economic_line_chart", vg_5).then(function(result) {
 }).catch(console.error);
